964c4566ba6e0ac65c3d9dd7ad7d879a8249d314
lana <lanaparkwinchester125@gmail.com>
2023-10-19 22:37:27 -0300

    aula 1 js

---
+++ script.js
@@ -0,0 +1,37 @@
+//let pessoa1 ='henrique';//essa variável e pode ser reatribuida
+//var pessoa2='joão';// esse tipo de varuiavel, não e mais utilizado como boas praticas 
+//const pessoa3 = 'lana';//a variavel do tipo constant, só piode serr atribuida uma unica vez
+
+//let mensagem = 'ola essa e a mensagem da minha variavel';
+//console.log(mensagem);
+//mensagem = 'essa e a nova mensagem';
+//console.log(mensagem)
+
+//floart: números que contem pontos ou virgulas
+// int:número inteiros(não contém pontos ou virgulas)
+//bool: booleano: true(verdadeiro), false(FALSO)
+//string: do tipo de texto, precisa ser colocado entre aspas duplas ou simples 
+
+
+let num1 =50;
+let num2 =20;
+//let soma = num1+num2;
+//console.log(soma)
+//let multi=num1 * num2;
+
+//console.log (multi);
+//let divisao =num1 / num2;
+//console.log(divisao);
+//let sub = num1 - num2;
+//console.log(sub)
+//if: se
+//: senão
+// >:maior que 
+
+if(num1 > 10 ){
+    console.log('o numero 1, é maior que 10');
+
+}  else{
+    console.log('o numero1,não e maior do que 10');
+}
+
